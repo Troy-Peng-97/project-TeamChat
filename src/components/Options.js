d705ab3e65bc61bbdbf73fb14797c1c77add0440
Troy Peng <troyqk@gmail.com>
2021-03-22 01:22:37 -0500

    modify logout button's style

--- src/components/Options.js
+++ src/components/Options.js
@@ -7,7 +7,7 @@ const Options = () => {
     return (
         <div className="options-container" align='center'>
             <button onClick={handleLogout} style={{width: '7vw', height: '2vw', backgroundColor: '#3B2A50', borderRadius: '12px'}}>
-                <div style={{color:'white', backgroundColor: '#3B2A50'}}> Logout</div>
+                 <div style={{color: "white"}}>Logout</div>
             </button> 
         </div>
     )
